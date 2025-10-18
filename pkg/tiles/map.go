f04cc24d7189efba9596d4aa0f5a3846ec2a3c89
Denis <korbakovd@gmail.com>
2025-10-18 18:53:58 +0300

    feat: add logic for map

---
+++ pkg/tiles/map.go
@@ -0,0 +1,30 @@
+package tiles
+
+import "fmt"
+
+type Format string
+
+var (
+	MBT Format = "mbt"
+)
+
+type Map struct {
+	File string
+}
+
+func NewMap(file string) *Map {
+	return &Map{
+		File: file,
+	}
+}
+
+// @todo add base logic
+func (m *Map) Convert(format Format) (*Map, error) {
+	switch format {
+	case MBT:
+		// @todo add use converter for map
+		return NewMap(m.File), nil
+	}
+
+	return nil, fmt.Errorf("unknown format: %s", format)
+}
