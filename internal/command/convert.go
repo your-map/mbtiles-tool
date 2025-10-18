295a9832c70421d8a22583bb8a5858253882269d
Denis <korbakovd@gmail.com>
2025-10-18 12:16:35 +0300

    fix: update cmd used form

--- internal/command/convert.go
+++ internal/command/convert.go
@@ -3,8 +3,9 @@ package command
 import (
 	"github.com/spf13/cobra"
 	"github.com/your-map/mbtiles-tool/configs/constname"
-	"github.com/your-map/mbtiles-tool/internal/component/form"
 	"github.com/your-map/mbtiles-tool/internal/component/output"
+
+	convertForm "github.com/your-map/mbtiles-tool/internal/component/convert"
 )
 
 // convertCmd Command for build pipeline
@@ -13,13 +14,12 @@ var convertCmd = &cobra.Command{
 	Short: constname.ShortConvertCmd,
 	Long:  constname.LongConvertCmd,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fields, err := form.Run()
+		fields, err := convertForm.Run()
 		if err != nil {
 			return err
 		}
 
-		output.Green("Success green output: ", fields.ExampleInput)
-		output.Red("This command will be run successfully")
+		output.Green("Success pick file: ", fields.File)
 
 		return nil
 	},
