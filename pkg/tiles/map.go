34bbe80abbba8137efec46c32907593827a1d603
Denis <korbakovd@gmail.com>
2025-10-20 21:17:53 +0300

    feat: add logic read base osm pbf file

--- pkg/tiles/map.go
+++ pkg/tiles/map.go
@@ -1,9 +1,15 @@
 package tiles
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/your-map/mbtiles-tool/internal/osmpbf"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
@@ -27,12 +33,41 @@ func NewMap(file string) *Map {
 func (m *Map) Convert(format Format) (*Map, error) {
 	switch format {
 	case MBT:
-		data, err := os.ReadFile(m.File)
+		file, err := os.Open(m.File)
 		if err != nil {
 			return nil, ErrReadFile
 		}
+		defer file.Close()
 
-		fmt.Println(string(data))
+		buf := new(bytes.Buffer)
+
+		buf.Reset()
+		if _, err = io.CopyN(buf, file, 4); err != nil {
+			return nil, ErrReadFile
+		}
+
+		buf.Reset()
+		if _, err = io.CopyN(buf, file, int64(binary.BigEndian.Uint32(buf.Bytes()))); err != nil {
+			return nil, err
+		}
+
+		blobHeader := new(osmpbf.BlobHeader)
+		if err := proto.Unmarshal(buf.Bytes(), blobHeader); err != nil {
+			return nil, err
+		}
+
+		buf.Reset()
+		if _, err = io.CopyN(buf, file, int64(blobHeader.GetDatasize())); err != nil {
+			return nil, err
+		}
+
+		blob := new(osmpbf.Blob)
+		if err = proto.Unmarshal(buf.Bytes(), blob); err != nil {
+			return nil, err
+		}
+
+		fmt.Println("Blob header: ", blobHeader)
+		fmt.Println("blob: ", blob)
 
 		return NewMap(m.File), nil
 	}
