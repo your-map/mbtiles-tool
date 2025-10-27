84907b149fb19a0de9a9b70423c1f727e8ced6d3
Denis <korbakovd@gmail.com>
2025-10-27 22:08:18 +0300

    feat: add logic for init db

--- internal/mbt/mbt.go
+++ internal/mbt/mbt.go
@@ -1,1 +1,33 @@
 package mbt
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Init() error {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		return err
+	}
+	defer func(db *sql.DB) {
+		err = db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+
+	query, err := os.ReadFile("internal/mbt/schema.sql")
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(string(query))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
