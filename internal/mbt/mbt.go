bbd1112b28d2f1adcd8af5296c193da6ebc9c7cb
Denis <korbakovd@gmail.com>
2025-10-29 21:07:00 +0300

    fix: update logic set metadata

--- internal/mbt/mbt.go
+++ internal/mbt/mbt.go
@@ -2,6 +2,8 @@ package mbt
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -52,7 +54,66 @@ func (m *MBT) WriteMetaData(metaData *proto.HeaderBlock) error {
 		}
 	}
 
+	_, err = stmt.Exec("version", "1.3")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("format", "pbf")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("type", "overlay")
+	if err != nil {
+		return err
+	}
+
+	bounds, center, err := executeGridMap(metaData.Bbox)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("bounds", bounds)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("center", center)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("minzoom", "0")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec("maxzoom", "14")
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func executeGridMap(bbox *proto.HeaderBBox) (string, string, error) {
+	if bbox == nil {
+		return "", "", errors.New("empty bbox")
+	}
+
+	left := float64(bbox.GetLeft()) / 1e9
+	right := float64(bbox.GetRight()) / 1e9
+	top := float64(bbox.GetTop()) / 1e9
+	bottom := float64(bbox.GetBottom()) / 1e9
+
+	centerLon := (left + right) / 2
+	centerLat := (bottom + top) / 2
+
+	bounds := fmt.Sprintf("%f,%f,%f,%f", left, bottom, right, top)
+	center := fmt.Sprintf("%f,%f,%d", centerLon, centerLat, 10)
+
+	return bounds, center, nil
 }
 
 func (m *MBT) Close() error {
